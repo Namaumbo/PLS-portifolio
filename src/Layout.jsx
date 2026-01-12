1ce59aed7f278a8667985dbd2ab81a10c60925c4
Namaumbo <daelo.namaumbo@gmail.com>
2026-01-12 23:03:08 +0200

    Enhance layout and styling by adding fixed background attachment to the main container, and implement text shadows for headings in CSS for improved visual appeal. Update NavBar to be sticky for better navigation experience.

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,13 +7,22 @@ import FooterComponent from "./components/footer";
 const Layout = () => {
   return (
     <>
-      <NavBarComponent />
-      <div className="container mx-auto p-5">
-        <Outlet />
-      </div>
-
-      <div className="mt-10">
-        <FooterComponent />
+      <div
+        className="bg-black"
+        style={{
+          background:
+            "rgb(0, 0, 0) radial-gradient(rgba(72, 72, 72, 0.712) 10%, transparent 1%)",
+          backgroundSize: "11px 11px",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        <NavBarComponent />
+        <div className="container mx-auto p-5">
+          <Outlet />
+        </div>
+        <div className="mt-10">
+          <FooterComponent />
+        </div>
       </div>
     </>
   );
